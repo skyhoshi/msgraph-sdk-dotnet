d16426899a93a253ec955464de0ee0b74b7e179f
Eastman <andrueastman@users.noreply.github.com>
2019-11-05 08:24:12 +0300

    Fix for improperly encoded string in Drive (#572)

--- tests/Microsoft.Graph.DotnetCore.Test/Requests/Extensions/DriveItemRequestBuilderExtensionsTests.cs
+++ tests/Microsoft.Graph.DotnetCore.Test/Requests/Extensions/DriveItemRequestBuilderExtensionsTests.cs
@@ -50,5 +50,28 @@ namespace Microsoft.Graph.DotnetCore.Test.Requests.Extensions
             Assert.NotNull(itemRequest);
             Assert.Equal(expectedRequestUri, new Uri(itemRequest.RequestUrl));
         }
+
+
+        // These tests are from the OneDrive docs found here to verify correct functionality
+        // https://docs.microsoft.com/en-us/onedrive/developer/rest-api/concepts/addressing-driveitems?view=odsp-graph-online#examples-1
+        [Theory]
+        [InlineData("Test#1.txt", "Test%231.txt")]
+        [InlineData("Ryan's Files", "Ryan's%20Files")]
+        [InlineData("doc (1).docx", "doc%20(1).docx")]
+        [InlineData("estimate%s.docx", "estimate%25s.docx")]
+        [InlineData("Break#Out", "Break%23Out")]
+        [InlineData("saved_game[1].bin", "saved_game[1].bin")]
+        public void ItemByPath_BuildRequestWithSpecialPoundCharacter(string pathInput, string expectedEncodedPath)
+        {
+            var expectedRequestUri = new Uri(string.Format(Constants.Url.GraphBaseUrlFormatString, "v1.0") + "/me/drive/root:/" + expectedEncodedPath + ":");
+            var itemRequestBuilder = this.graphServiceClient.Me.Drive.Root.ItemWithPath(pathInput) as DriveItemRequestBuilder;
+
+            Assert.NotNull(itemRequestBuilder);
+            Assert.Equal(expectedRequestUri, new Uri(itemRequestBuilder.RequestUrl));
+
+            var itemRequest = itemRequestBuilder.Request() as DriveItemRequest;
+            Assert.NotNull(itemRequest);
+            Assert.Equal(expectedRequestUri, new Uri(itemRequest.RequestUrl));
+        }
     }
 }
