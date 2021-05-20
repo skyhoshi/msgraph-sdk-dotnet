0b5bf0e0cbc9ca556c8b28e446428a92d7aa8d5e
Michael Mainer <MIchaelMainer@users.noreply.github.com>
2021-05-20 14:59:39 -0700

    V1.0/pipelinebuild/49768 (#1008) (#1012)
    * Update generated files with build 49768

--- src/Microsoft.Graph/Generated/model/B2xIdentityUserFlow.cs
+++ src/Microsoft.Graph/Generated/model/B2xIdentityUserFlow.cs
@@ -58,6 +58,12 @@ namespace Microsoft.Graph
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "userAttributeAssignments", Required = Newtonsoft.Json.Required.Default)]
         public IB2xIdentityUserFlowUserAttributeAssignmentsCollectionPage UserAttributeAssignments { get; set; }
     
+        /// <summary>
+        /// Gets or sets user flow identity providers.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "userFlowIdentityProviders", Required = Newtonsoft.Json.Required.Default)]
+        public IB2xIdentityUserFlowUserFlowIdentityProvidersCollectionWithReferencesPage UserFlowIdentityProviders { get; set; }
+    
     }
 }
 
